9bd9539f693afec86b1df9f13755f4e613344ae3
NahlaHussein1 <nahlahussein518@gmail.com>
2023-04-26 15:16:52 -0700

    d

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-/i*
+/*
  * File: _printf.c
  * Author: NahlaHussein
  *
@@ -10,7 +10,9 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
+ *
  * @format: format.
+ *
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -60,8 +62,11 @@ int _printf(const char *format, ...)
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
+ *
  * @buffer: Array of chars
+ *
  * @buff_ind: Index at which to add next char, represents the length.
+ *
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
